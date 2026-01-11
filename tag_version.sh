3a12c454750166a42d107860dae152e0f639ff53
danhumassmed <daniel.higgins@umassmed.edu>
2026-01-11 14:58:26 -0500

    bug fix

--- tag_version.sh
+++ tag_version.sh
@@ -2,11 +2,13 @@
 VERSION_FILE="launch_jupyter.src" 
 COMMAND_FILE="launch_jupyter.command"
 
+VERSION_LINE=$(grep VERSION_LINE $VERSION_FILE)
+VERSION=$(echo "$VERSION_LINE" | sed 's/.*CURRENT_VERSION=v\([0-9.]*\).*/\1/')
+
 # Git commit, tag, and push
 git add "$VERSION_FILE"
 git add "$COMMAND_FILE"
-git commit -m "Bump version to $NEW_VERSION"
-git tag "v$NEW_VERSION"
+git commit -m "Bump version to $VERSION"
+git tag "v$VERSION"
 git push
 git push --tags
-
